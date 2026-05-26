8f4498bf5599d3eba2b3681527714da56c532fe2
Karol <91410360+TurystaKarolek@users.noreply.github.com>
2026-05-26 20:08:10 +0200

    Create logger.py

---
+++ kernel/logger.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+
+class Logger:
+    def info(self, message):
+        print(f"[INFO] {datetime.now()} | {message}")
+
+    def warning(self, message):
+        print(f"[WARNING] {datetime.now()} | {message}")
+
+    def error(self, message):
+        print(f"[ERROR] {datetime.now()} | {message}")
