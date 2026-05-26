01a7de83cb200b5be46a32f1fd0818ecfcb8f33c
Karol <91410360+TurystaKarolek@users.noreply.github.com>
2026-05-26 16:43:48 +0200

    Update echo_link.py

--- modules/echo_link.py
+++ modules/echo_link.py
@@ -7,3 +7,6 @@ class EchoLink:
 
     def shutdown(self):
         print("[Echo-Link] Module shutdown.")
+
+    def on_system_start(self, data):
+        print("[Echo-Link] System startup event received.")
