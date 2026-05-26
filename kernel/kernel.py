30808da8fa4c65264075208ef9af454ce8d22587
Karol <91410360+TurystaKarolek@users.noreply.github.com>
2026-05-26 16:31:59 +0200

    Update kernel.py

--- kernel/kernel.py
+++ kernel/kernel.py
@@ -4,12 +4,23 @@ class Kernel:
 
     def register_module(self, module):
         self.modules[module.name] = module
+
+        if hasattr(module, "initialize"):
+            module.initialize()
+
         print(f"[+] Module loaded: {module.name}")
 
     def unload_module(self, module_name):
         if module_name in self.modules:
+            module = self.modules[module_name]
+
+            if hasattr(module, "shutdown"):
+                module.shutdown()
+
             del self.modules[module_name]
+
             print(f"[-] Module unloaded: {module_name}")
+
         else:
             print("[!] Module not found.")
 
