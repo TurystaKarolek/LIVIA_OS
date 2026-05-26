0a28fb30b3c1fb4b963aac19bcdbd7a1b20c7225
Karol <91410360+TurystaKarolek@users.noreply.github.com>
2026-05-26 20:10:27 +0200

    Add runtime logging system

--- kernel/kernel.py
+++ kernel/kernel.py
@@ -1,19 +1,21 @@
 from kernel.events import EventBus
 from kernel.context import RuntimeContext
+from kernel.logger import Logger
 
 class Kernel:
     def __init__(self):
         self.modules = {}
         self.events = EventBus()
         self.context = RuntimeContext()
-
+        self.logger = Logger()
+        
     def register_module(self, module):
         self.modules[module.name] = module
 
         if hasattr(module, "initialize"):
             module.initialize()
 
-        print(f"[+] Module loaded: {module.name}")
+        self.logger.info(f"Module loaded: {module.name}")
 
     def unload_module(self, module_name):
         if module_name in self.modules:
@@ -24,11 +26,11 @@ class Kernel:
 
             del self.modules[module_name]
 
-            print(f"[-] Module unloaded: {module_name}")
+            self.logger.info(f"Module unloaded: {module_name}")
 
         else:
-            print("[!] Module not found.")
-
+            self.logger.warning("Module not found.")
+            
     def list_modules(self):
         return list(self.modules.keys())
 
